00f88c0e578e144df4e12eb75fa7d3a4511650a3
Jordan Mahsman <31456960+Mahsman27@users.noreply.github.com>
2021-01-11 22:03:00 -0600

    Add project files.

---
+++ digitalClock/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace digitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDays = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Impact", 69.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Transparent;
+            this.lblTime.Location = new System.Drawing.Point(231, 109);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(256, 115);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "22:22";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Impact", 30F);
+            this.lblDate.ForeColor = System.Drawing.Color.Transparent;
+            this.lblDate.Location = new System.Drawing.Point(441, 260);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(268, 48);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "January 11 2021";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDays
+            // 
+            this.lblDays.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblDays.AutoSize = true;
+            this.lblDays.Font = new System.Drawing.Font("Impact", 30F);
+            this.lblDays.ForeColor = System.Drawing.Color.Transparent;
+            this.lblDays.Location = new System.Drawing.Point(253, 260);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(149, 48);
+            this.lblDays.TabIndex = 3;
+            this.lblDays.Text = "Monday";
+            this.lblDays.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.Black;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Impact", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(807, -15);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(138, 83);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Black;
+            this.lblName.Font = new System.Drawing.Font("Impact", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(288, 60);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Digital Clock";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.ForeColor = System.Drawing.Color.White;
+            this.lblSeconds.Location = new System.Drawing.Point(582, 168);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(140, 43);
+            this.lblSeconds.TabIndex = 6;
+            this.lblSeconds.Text = "seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(915, 430);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblDays);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTime);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSeconds;
+    }
+}
+
---
+++ digitalClock/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace digitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            lblTime.Text = DateTime.Now.ToString("t");
+            lblSeconds.Text = DateTime.Now.ToString("ss");
+            lblDate.Text = DateTime.Now.ToString("MMM, dd yyyy");
+            lblDays.Text = DateTime.Now.ToString("dddd");
+
+            btnClose.TabStop = false;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatAppearance.BorderColor = Color.FromArgb(0, 255, 255, 255);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
